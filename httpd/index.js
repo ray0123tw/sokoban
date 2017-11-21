77bf0b1d349856ef4f080dfc6464f91788d79d85
ray0123tw <ray0123tw@yahoo.com.tw>
2017-11-21 19:02:18 +0800

    1121-1

--- httpd/index.js
+++ httpd/index.js
@@ -2,20 +2,66 @@
 
  let http = require('http');
 
- http.createServer((request, response) => {
- // 傳送 HTTP header
+ const routingTable = require('./config.json');
 
- // HTTP Status: 200 : OK
- // Content Type: text/plain
+ /**
+ * 利⽤ http.ServerResponse 物件回傳檔案內容
+ *
+ * @name serve
+ * @function
+ * @param response - http.ServerResponse 物件
+ * @param fname - 要回傳的檔案名
+ * @param datatype - 回傳檔案內容的 Mine-Type
+ * @returns {undefined}
+ */
+ let serve = (response, fname, datatype) => {
+ let fs = require('fs');
+
+ fs.readFile(fname, (err, data) => {
+ if (err) {
+ console.log(' 檔案讀取錯誤');
+ }
+ else {
  response.writeHead(200, {
- 'Content-Type': 'text/plain'
+ 'Content-Type': datatype
  });
 
- // 傳送回應內容。
- response.end('Hello World!\n');
+ response.write(data);
+ response.end();
+ }
+ });
+ };
 
- console.log('request.headers: \n', request.headers)
+ http.createServer((request, response) => {
+ let fs = require('fs');
+
+ let postData = '';
+
+ // 利⽤ 'data' event 消耗掉 data chunk;
+ // 'end' event 才會被 fired
+ request.on('data', (chunk) => {
+ postData += chunk;
+
+ console.log(
+ ' 接收的 POST data ⽚段 k: [' + chunk + '].'
+ );
+ });
+
+ request.on('end', () => {
+ if (request.url in routingTable) {
+ let obj = routingTable[request.url];
+
+ serve(response, obj.url, obj.type);
+ }
+ else {
+ console.log(' 未定義的存取: ' + request.url);
+
+ response.end();
+ }
+ });
  }).listen(8088);
 
  // log message to Console
  console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
+ // index.js
